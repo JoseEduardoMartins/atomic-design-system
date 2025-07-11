fbcb4de40a79556e7c253578bea113b5d07afe02
Eduardo Martins <m4rt1ns.jose@gmail.com>
2025-07-11 18:31:32 -0300

    fix: update Storybook 9.0 configuration and remove deprecated addons
    - Remove @storybook/addon-controls, @storybook/addon-viewport, @storybook/addon-backgrounds
    - Update .storybook/main.ts to use only valid addons
    - Update .storybook/preview.ts with new configuration syntax
    - Remove deprecated dependencies from package.json
    - Fix Storybook loading issues with infinite loading

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,10 +7,7 @@ const config: StorybookConfig = {
     '@chromatic-com/storybook',
     '@storybook/addon-docs',
     '@storybook/addon-a11y',
-    '@storybook/addon-vitest',
-    '@storybook/addon-controls',
-    '@storybook/addon-viewport',
-    '@storybook/addon-backgrounds'
+    '@storybook/addon-vitest'
   ],
   framework: {
     name: '@storybook/react-vite',
